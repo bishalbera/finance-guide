771460ae9afa976d0f654b2198dd4862fc9415e6
bishalbera <bishalbera2@gmail.com>
2024-09-15 15:51:30 +0530

    modified the api and added some utility fun

--- app/api/finance-guide/route.ts
+++ app/api/finance-guide/route.ts
@@ -1,36 +1,60 @@
+import {
+  FinanceAdvisorAIStream,
+  FinanceAIStreamPayload,
+} from "@/utils/financeAdvisor";
 import { NextRequest, NextResponse } from "next/server";
 import OpenAI from "openai";
 
-export const POST = async (req: NextRequest, res: NextResponse) => {
+// export const POST = async (req: NextRequest, res: NextResponse) => {
+//   try {
+//     const client = new OpenAI({
+//       baseURL: process.env.BASE_URL as string,
+//       apiKey: "",
+//     });
+
+//     const { advisor, message } = await req.json();
+
+//     const aiResponse = await client.chat.completions.create({
+//       model: "Meta-Llama-3-8B-Instruct-Q5_K_M",
+//       messages: [
+//         {
+//           role: "system",
+//           content: `You are ${advisor}. Someone is asking you for financial advice. Respond in your personal style.`,
+//         },
+//         {
+//           role: "user",
+//           content: message,
+//         },
+//       ],
+
+//       temperature: 0.7,
+//       max_tokens: 500,
+//     });
+//     return NextResponse.json({
+//       response: aiResponse.choices[0].message.content,
+//     });
+//   } catch (error) {
+//     console.error("an error occured", error);
+//     return NextResponse.json("some error occurred", { status: 500 });
+//   }
+// };
+
+export const POST = async (req: NextRequest) => {
+  const { messages } = await req.json();
+
   try {
-    const client = new OpenAI({
-      baseURL: process.env.BASE_URL as string,
-      apiKey: "",
-    });
-
-    const { advisor, message } = await req.json();
-
-    const aiResponse = await client.chat.completions.create({
-      model: "Meta-Llama-3-8B-Instruct-Q5_K_M",
-      messages: [
-        {
-          role: "system",
-          content: `You are ${advisor}. Someone is asking you for financial advice. Respond inyour personal style.`,
-        },
-        {
-          role: "user",
-          content: message,
-        },
-      ],
-
+    const payload: FinanceAIStreamPayload = {
+      model: "llama",
+      messages,
+      stream: true,
       temperature: 0.7,
-      max_tokens: 500,
-    });
-    return NextResponse.json({
-      response: aiResponse.choices[0].message.content,
-    });
+    };
+    const stream = await FinanceAdvisorAIStream(payload);
+    return NextResponse.json(stream);
   } catch (error) {
-    console.error("an error occured", error);
-    return NextResponse.json("some error occurred", { status: 500 });
+    return NextResponse.json(
+      { message: "Error, answer stream failed" },
+      { status: 500 }
+    );
   }
 };
